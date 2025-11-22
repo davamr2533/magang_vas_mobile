efb218b8d95e5c3284845ee37a30e4f3ef184732
davamr2533 <davamr@students.amikom.ac.id>
2025-11-22 20:00:09 +0700

    refresh indicator

--- lib/screen/task_track/task_track_vas/track_vas_page.dart
+++ lib/screen/task_track/task_track_vas/track_vas_page.dart
@@ -88,132 +88,139 @@ class _TrackVasPage extends State<TrackVasPage> {
                 return task.jenis.toLowerCase().contains(_searchQuery.toLowerCase());
               }).toList();
 
-              return Column(
-                children: [
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
+              return RefreshIndicator(
+                onRefresh: () async {
+                  await context.read<TaskTrackCubit>().fetchTask();
+                },
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
 
-                      // Search Bar
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.only(left: 16, right: 8),
-                          child: TextField(
-                            onChanged: (value) {
-                              setState(() {
-                                _searchQuery = value;
-                              });
-                            },
-                            style: GoogleFonts.urbanist(),
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: pinkNewAmikom,
-                              hintText: 'Cari Task...',
-                              hintStyle: GoogleFonts.urbanist(
-                                color: darkGrayNewAmikom,
-                              ),
-                              contentPadding: const EdgeInsets.symmetric(
-                                  vertical: 12, horizontal: 16),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.circular(14),
-                              ),
-                              suffixIcon: Padding(
-                                padding: const EdgeInsets.only(right: 8),
-                                child: Icon(
-                                  Icons.search_rounded,
+                        // Search Bar
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 16, right: 8),
+                            child: TextField(
+                              onChanged: (value) {
+                                setState(() {
+                                  _searchQuery = value;
+                                });
+                              },
+                              style: GoogleFonts.urbanist(),
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: pinkNewAmikom,
+                                hintText: 'Cari Task...',
+                                hintStyle: GoogleFonts.urbanist(
                                   color: darkGrayNewAmikom,
-                                  size: 24,
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 12, horizontal: 16),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(14),
+                                ),
+                                suffixIcon: Padding(
+                                  padding: const EdgeInsets.only(right: 8),
+                                  child: Icon(
+                                    Icons.search_rounded,
+                                    color: darkGrayNewAmikom,
+                                    size: 24,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
 
-                      // History Button
-                      Container(
-                        width: 50,
-                        height: 50,
-                        margin: const EdgeInsets.only(right: 16),
-                        decoration: BoxDecoration(
-                          color: yellowNewAmikom,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.history_rounded,
-                            color: Colors.blue,
-                            size: 28,
+                        // History Button
+                        Container(
+                          width: 50,
+                          height: 50,
+                          margin: const EdgeInsets.only(right: 16),
+                          decoration: BoxDecoration(
+                            color: yellowNewAmikom,
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                          onPressed: () {
-                            Navigator.of(context).pushReplacement(
-                                routingPage(
-                                  BlocProvider(
-                                    create: (context) => TaskHistoryCubit(TaskTrackService()),
-                                    child: TrackVasHistory(),
-                                  ),
-                                )
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.history_rounded,
+                              color: Colors.blue,
+                              size: 28,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pushReplacement(
+                                  routingPage(
+                                    BlocProvider(
+                                      create: (context) => TaskHistoryCubit(TaskTrackService()),
+                                      child: TrackVasHistory(),
+                                    ),
+                                  )
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    // Jika hasil filter kosong, tetap tampilkan search bar & tombol tapi halamannya kosong
+                    if (filteredTasks.isEmpty)
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            "Tidak ada Task",
+                            style: GoogleFonts.urbanist(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      )
+                    else
+                      Expanded(
+                        child: ListView.builder(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          itemCount: filteredTasks.length + 1,
+                          itemBuilder: (context, index) {
+                            if (index == filteredTasks.length) {
+                              return const SizedBox(height: 80);
+                            }
+
+                            final task = filteredTasks[index];
+
+                            //Flow Progress
+                            final Map<String, String> progressFlow = {
+                              "Wawancara": "Konfirmasi Desain",
+                              "Konfirmasi Desain": "Perancangan Database",
+                              "Perancangan Database": "Pengembangan Software",
+                              "Pengembangan Software": "Debugging",
+                              "Debugging": "Testing",
+                              "Testing": "Trial",
+                              "Trial": "Production",
+                              "Production": "-",
+                              "-": "Wawancara", //bahan untuk test
+                            };
+
+                            final String nextProgress = progressFlow[task.currentProgress] ?? "-";
+
+                            return TrackVasCard(
+                              task: task,
+                              nextProgress: nextProgress,
+                              catatanController: _catatanController,
                             );
                           },
                         ),
                       ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 30),
-
-                  // Jika hasil filter kosong, tetap tampilkan search bar & tombol tapi halamannya kosong
-                  if (filteredTasks.isEmpty)
-                    Expanded(
-                      child: Center(
-                        child: Text(
-                          "Tidak ada Task",
-                          style: GoogleFonts.urbanist(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    )
-                  else
-                    Expanded(
-                      child: ListView.builder(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        itemCount: filteredTasks.length + 1,
-                        itemBuilder: (context, index) {
-                          if (index == filteredTasks.length) {
-                            return const SizedBox(height: 80);
-                          }
-
-                          final task = filteredTasks[index];
-
-                          //Flow Progress
-                          final Map<String, String> progressFlow = {
-                            "Wawancara": "Konfirmasi Desain",
-                            "Konfirmasi Desain": "Perancangan Database",
-                            "Perancangan Database": "Pengembangan Software",
-                            "Pengembangan Software": "Debugging",
-                            "Debugging": "Testing",
-                            "Testing": "Trial",
-                            "Trial": "Production",
-                            "Production": "-",
-                            "-": "Wawancara", //bahan untuk test
-                          };
-
-                          final String nextProgress = progressFlow[task.currentProgress] ?? "-";
-
-                          return TrackVasCard(
-                            task: task,
-                            nextProgress: nextProgress,
-                            catatanController: _catatanController,
-                          );
-                        },
-                      ),
-                    ),
-                ],
+                  ],
+                )
               );
+
+
             }
             return const SizedBox();
           },
