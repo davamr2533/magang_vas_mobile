d43470a61e1979ec6c43d09ee7891adcaf766f31
davamr2533 <davamr@students.amikom.ac.id>
2025-11-25 09:17:00 +0700

    catatan bisa di scroll

--- lib/screen/task_track/task_track_user/track_user_page.dart
+++ lib/screen/task_track/task_track_user/track_user_page.dart
@@ -114,7 +114,7 @@ class _TrackUserPage extends State<TrackUserPage> {
                     context.read<TaskTrackCubit>().fetchTask();
                   },
                   child: ListView.builder(
-                      padding: const EdgeInsets.all(16),
+                      padding: const EdgeInsets.fromLTRB(16, 16, 16, 40),
                       itemCount: tasks.length,
                       itemBuilder: (context, index) {
                         final task = tasks[index];
